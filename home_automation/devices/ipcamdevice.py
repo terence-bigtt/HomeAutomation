94102279897add7e13a0733366ad072707d4e7c7
terence <teereence@gmail.com>
2017-07-08 21:46:09 +0200

    IpCam device and device class

---
+++ home_automation/devices/ipcamdevice.py
@@ -0,0 +1,33 @@
+from home_automation.devices.device import Device
+import uuid
+import requests
+
+
+
+class IpCamDevice(Device) :
+    def __init__(self, name, location, device_id=uuid.uuid4(), ip=None, cam_user=None, cam_password=None,
+                 capture_path=None, payload=None):
+        """
+        Driver for IpCam type devices.
+        :param name: name of the device
+        :param location: physical location
+        :param device_id: unique identifier for the device
+        :param ip: ip on the network
+        :param cam_user: user name for accessing the cam
+        :param cam_password: password for accessing the cam
+        :param capture_path: address to call for getting a snapshot
+        :payload parameters to pass to the capture url
+        """
+        Device.__init__(self, name, "ipcam", location, device_id)
+        self.ip = ip
+        self.cam_user = cam_user
+        self.cam_password = cam_password
+        self.capture_path = capture_path
+        self.payload = payload
+
+    def run(self):
+        capture_url = self.capture_path + "/" + self.capture_path
+        req = requests.get(capture_url, params=self.payload)
+        content = req.content
+        raw = req.raw
+        return content
--- home_automation/views/general.py
+++ home_automation/views/general.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request
 from home_automation.controllers.general.main_menu import get_configured_menu
-from jinja2 import Undefined
 
 mod = Blueprint('general', __name__)
 
@@ -12,3 +11,4 @@ def index():
     template_args = {"menu": menu.items, "iframe_href": iframe_href}
     filtered_args = {k: template_args.get(k) for k in template_args.keys() if template_args.get(k) is not None}
     return render_template('general/index.html', **filtered_args)
+
