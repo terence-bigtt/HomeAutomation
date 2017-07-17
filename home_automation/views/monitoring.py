9a9381c642b0425c7fcc77c2149de0f30a8eb31e
terence <terence.delsate>
2017-07-17 21:22:41 +0200

    minor fix video stream

--- home_automation/views/monitoring.py
+++ home_automation/views/monitoring.py
@@ -10,27 +10,31 @@ def stream(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-def preview(camera) :
+
+def preview(camera):
     return camera.run()
+
 
 @monitoring.route("/preview")
 def video_preview():
     cam_id = request.args.get("id")
     cams = get_configured_ip_cams()
-    cam = filter(lambda c: c.device_id == cam_id,reduce(lambda c1, c2: c1 + c2 , cams, []))
+    cam = filter(lambda c: c.device_id == cam_id, reduce(lambda c1, c2: c1 + c2, cams, []))
 
     try:
-        return Response(preview(cam))
+        return Response(preview(cam[0]))
     except Exception as e:
-         return send_file("static/image/noimage.jpg", mimetype='image/jpg')
+        return send_file("static/image/noimage.jpg", mimetype='image/jpg')
+
 
 @monitoring.route("/video_feed")
 def video_feed():
     cam_id = request.args.get("id")
-    cam = get_configured_ip_cams().get(cam_id)
-    try :
-        return Response(stream(cam),
+    cams = get_configured_ip_cams()
+    cam = filter(lambda c: c.device_id == cam_id, reduce(lambda c1, c2: c1 + c2, cams, []))
+
+    try:
+        return Response(stream(cam[0]),
                         mimetype='multipart/x-mixed-replace; boundary=frame')
     except Exception as e:
-         return send_file("static/image/noimage.jpg", mimetype='image/jpg')
-
+        return send_file("static/image/noimage.jpg", mimetype='image/jpg')
