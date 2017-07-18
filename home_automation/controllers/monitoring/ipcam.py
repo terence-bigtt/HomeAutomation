e48f7cd4fbc5ba745fbda19bf9b8a443a3f5b2ee
terence <terence.delsate>
2017-07-18 15:41:57 +0200

    updated css for monitoring view

--- home_automation/controllers/monitoring/ipcam.py
+++ home_automation/controllers/monitoring/ipcam.py
@@ -5,9 +5,10 @@ from home_automation.private_config import ipcam_conf
 def get_configured_ip_cams():
     ipcam = []
     ipcam.append(IpCamDevice("Car Port", "under the carport", ip="192.168.1.55", capture_path="snapshot.cgi",
-                        payload=ipcam_conf["payload"], device_id="1"))
-    ipcam.append(IpCamDevice("Room1", "room at the floor", ip="192.168.1.50", cam_user= ipcam_conf["payload"].get("user"),
-                         capture_path="tmpfs/auto.jpg", cam_password= ipcam_conf["payload"].get("pwd"), device_id="2", authentication="BASIC"))
+                             payload=ipcam_conf["payload"], device_id="1"))
+    ipcam.append(
+        IpCamDevice("Room1", "room at the floor", ip="192.168.1.50", cam_user=ipcam_conf["payload"].get("user"),
+                    capture_path="tmpfs/auto.jpg", cam_password=ipcam_conf["payload"].get("pwd"), device_id="2",
+                    authentication="BASIC"))
 
     return [ipcam]
-
